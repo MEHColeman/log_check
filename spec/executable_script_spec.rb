ac03c447584520e98b07a2e31b5f79e6b9009189
Mark Coleman <m@rkcoleman.co.uk>
2022-10-20 20:01:00 +0100

    dev: Add feature spec for validation checks

--- spec/executable_script_spec.rb
+++ spec/executable_script_spec.rb
@@ -2,18 +2,53 @@
 
 # The executable script `parser.rb` takes one argument: filename and produces
 # simple stats analysis information.
+# An error message and usage information is displayed if no file is specified.
+# Only valid IP addresses are considered if the `--strict` option is given.
 
 module LogCheck
   RSpec.describe 'executable script' do
-    subject { `./parser.rb "#{log_file_location}"` }
+    subject { `./parser.rb #{optional_args} #{log_file_location}` }
+    let(:optional_args) { '' }
 
-    context 'with a short, well-formed example log file' do
+    context 'with a short example log file' do
       let(:log_file_location) { 'spec/fixtures/simple_test.log' }
 
       it 'outputs a correct view count' do
         expect(subject).to start_with(
           "/help_page/1 5 visits\n/test 3 visits\n/help_page/2 1 visits"
         )
+      end
+    end
+
+    context 'without a log file given' do
+      let(:log_file_location) { '' }
+
+      it 'outputs an error and usage information' do
+        expect(subject).to start_with(
+          "Usage:"
+        )
+      end
+    end
+
+    context 'when passed the --strict option' do
+      let(:optional_args) { '--strict ' }
+
+      context 'when the file has some valid IP addresses' do
+        let(:log_file_location) { 'spec/fixtures/bad_ip_ranges_test.log' }
+
+        it 'outputs a correct, validated view count' do
+          expect(subject).to start_with(
+            "/help_page/1 3 visits\n/test 1 visits"
+          )
+        end
+      end
+
+      context 'when the file has no valid IP addresses' do
+        let(:log_file_location) { 'spec/fixtures/simple_test.log' }
+
+        it 'outputs no text, if the validated view count is 0' do
+          expect(subject).to eq('')
+        end
       end
     end
   end
