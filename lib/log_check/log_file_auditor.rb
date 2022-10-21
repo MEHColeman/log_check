e11fe061cb2f8117c9d686762a99df1195f9308f
Mark Coleman <m@rkcoleman.co.uk>
2022-10-21 16:21:26 +0100

    dev: Memoize Auditor data and implement counts
    Collate and memoize the visit data in one place, and create two counting
    methods that will count total visits or unique visits.

--- lib/log_check/log_file_auditor.rb
+++ lib/log_check/log_file_auditor.rb
@@ -2,32 +2,49 @@
 
 require_relative 'validators/simple_validator'
 
+## This Auditor will collate the information from the file whilst applying a
+# validation filter. This collated visit data is memoised, so that it can be
+# counted in two ways.
 module LogCheck
   class LogFileAuditor
     def initialize(file_location: ,
                    validator: SimpleValidator)
       @file_location = file_location
       @validator = validator
-      @visits = Hash.new(0)
     end
 
     def count_total_views
+      collate_visits.
+        map{|url, ip_visits| [url, ip_visits.
+                              map{|ip, count| count}.
+                              reduce(:+)]}.
+      sort_by { |url, total_visits| -total_visits }
+    end
+
+    def count_unique_views
+      collate_visits.
+        map{|url, ip_visits| [url,ip_visits.count]}.
+        sort_by { |url, unique_views| -unique_views }
+    end
+
+    private
+    def collate_visits
+      return @visits if @visits
+
+      @visits = Hash.new { |h,k| h[k]= Hash.new(0) }
       begin
         File.readlines(@file_location).each do |line|
           @validator.line_validate!(line)
           if @validator.ip_valid?(line)
             url, ip_address = line.split ' '
-            @visits[url] += 1
+            @visits[url][ip_address] += 1
           end
         end
       rescue SystemCallError => _file_error
         $stderr.puts 'File error. File must be present and readable'
         raise RuntimeError, 'File not read.'
       end
-      @visits.sort_by { |url, visits| -visits }
-    end
-    def count_unique_views
-      []
+      @visits
     end
   end
 end
