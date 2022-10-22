0ecbe1be174880783406e5e0f57eecca729a9d74
Mark Coleman <m@rkcoleman.co.uk>
2022-10-22 17:55:26 +0100

    dev: Dry up Validators

--- lib/log_check/validators/simple_validator.rb
+++ lib/log_check/validators/simple_validator.rb
@@ -1,18 +1,19 @@
 #frozen_string_literal: true
 
 module LogCheck
-  SIMPLE_LINE_MATCHER=/(?<url>^\/[(\/|\w]+) (?<ip>(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3}))$/
+  LINE_MATCHER=/(?<url>^\/[(\/|\w]+) (?<ip>(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3}))$/
 
   ## SimpleValidator validates each line.
-  # It DOES NOT validate the ip_address for valid format.
-  # Checks the whole line for two and only two values.
+  # It DOES NOT validate the ip_address for valid octet values.
+  # Checks the whole line for two and only two values, the second being 4 sets
+  # of digits.
   class SimpleValidator
     def self.line_validate!(line)
-      fail RuntimeError, 'Logfile format error' unless line.match?(SIMPLE_LINE_MATCHER)
+      fail RuntimeError, 'Logfile format error' unless line.match?(LINE_MATCHER)
     end
 
     def self.ip_valid?(line)
-      line.match?(SIMPLE_LINE_MATCHER)
+      line.match?(LINE_MATCHER)
     end
   end
 end
--- lib/log_check/validators/strict_validator.rb
+++ lib/log_check/validators/strict_validator.rb
@@ -1,33 +1,31 @@
 #frozen_string_literal: true
 
-module LogCheck
-  STRICT_LINE_MATCHER=/(?<url>^\/[(\/|\w]+) (?<ip>(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3}))$/
+require_relative 'simple_validator'
 
+module LogCheck
   ## StrictValidator validates each line.
   # It DOES validate the ip_address for valid format.
   # Checks the whole line for two and only two values.
-  class StrictValidator
+  class StrictValidator < SimpleValidator
     def self.line_validate!(line)
-      fail RuntimeError, 'Logfile format error' unless line.match?(STRICT_LINE_MATCHER)
+      super
     end
 
     def self.ip_valid?(line)
-      match = line.match(STRICT_LINE_MATCHER)
-      if match
-        ip = match[:ip]
-        no_octet_is_gt_255(ip) && no_octet_has_leading_zeros(ip)
-      else
-        false
-      end
+      match = line.match(LINE_MATCHER)
+      return false unless match
+
+      ip = match[:ip]
+      no_octet_is_gt_255(ip) && no_octet_has_leading_zeros(ip)
     end
 
     private
     def self.no_octet_is_gt_255(ip)
-      ip.split('.').map{|n| n.to_i<256}.reduce(:&)
+      ip.split('.').map{|n| n.to_i < 256}.reduce(:&)
     end
 
     def self.no_octet_has_leading_zeros(ip)
-      ip.split('.').map{|n| n.length==1 || n[0]!='0'}.reduce(:&)
+      ip.split('.').map{|n| n.length == 1 || n[0]!='0'}.reduce(:&)
     end
   end
 end
